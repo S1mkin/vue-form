317b93fc0a62a84401636ba656fd9fbd89f6d033
S1mkin <s.a.s.s.e.r@mail.ru>
2019-10-09 11:01:45 +0300

    Add default files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <vForm />
+  </div>
+</template>
+
+<script>
+import vForm from './components/vForm.vue'
+
+export default {
+  name: 'v-form',
+  components: {
+    vForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/vForm.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div class="container">
+    
+    <form>
+        <div class="form-group">
+            <label>Email address</label>
+            <input 
+                v-model="d_email"
+                type="email" 
+                class="form-control"
+                placeholder="Enter email"
+            >
+            <small class="form-text text-muted">We'll never share your email with anyone else {{d_email}}.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input 
+                v-model="d_password"
+                type="password" 
+                class="form-control" 
+                id="exampleInputPassword1" 
+                placeholder="Password"
+            >
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'vForm',
+  data() {
+    return {
+        d_email: '',
+        d_password: ''
+    }
+  }
+}
+</script>
